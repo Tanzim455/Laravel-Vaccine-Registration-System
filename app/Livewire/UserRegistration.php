bbb4453369907f0e45bd8d4ddd62eb638c10bbd1
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-01-16 17:59:49 +0600

    User registration with name and unqiue email added

--- app/Livewire/UserRegistration.php
+++ app/Livewire/UserRegistration.php
@@ -2,16 +2,40 @@
 
 namespace App\Livewire;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use App\Models\VaccineCentre;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class UserRegistration extends Component
 {
-    public string $title;
-    public int $count=0;
-    public function increment(){
-       $this->count++;
+    
+    
+    public string $email;
+    
+    public  string   $name;
+    
+    public function save()
+    {
+        $validated=$this->validate(); 
+        // ...
+        User::create($validated);
+        $this->reset();
+        session()->flash('success', 'You have been successfully registered.Soon you will get an email with confirmation date');
     }
+ 
+    protected function rules(): array
+    {
+        return (new RegisterRequest())->rules();
+    }
+    
+    
+        
+
+    //     session()->flash('success', 'You have been successfully registered.Soon you will get an email with confirmation date');
+    // }
     public function render()
     {
         $vaccineCentres=VaccineCentre::select('id','name','daily_limit')->get();
--- resources/views/livewire/user-registration.blade.php
+++ resources/views/livewire/user-registration.blade.php
@@ -2,18 +2,25 @@
     {{-- Because she competes with no one, no one can compete with her. --}}
     <!-- Form Elements: Inputs -->
     <div class="flex flex-col justify-center items-center">
-
-        <form class="space-y-6 dark:text-gray-100 xl:w-1/6 " onsubmit="return false;">
+        @if (session()->has('success'))
+        <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50">
+            {{ session('success') }}
+        </div>
+          @endif
+        <form wire:submit.prevent="save" class="space-y-6 dark:text-gray-100 xl:w-1/6 ">
             <!-- Text Input -->
             <div class="space-y-1">
               <label for="name" class="font-medium">Name</label>
               <input
                 type="text"
-                id="firstname"
-                name="firstname"
+                
+                wire:model="name"
                 placeholder="Enter your name"
                 class="block w-full rounded-lg border border-gray-200 px-3 py-2 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500"
               />
+              @error('name')
+              <div class="mt-1 text-red-500">{{ $message }}</div>
+          @enderror
             </div>
             <!-- END Text Input -->
           
@@ -22,40 +29,25 @@
               <label for="email" class="font-medium">Email</label>
               <input
                 type="email"
-               
-                name="email"
+                   wire:model="email"
                 placeholder="Enter your email"
                 class="block w-full rounded-lg border border-gray-200 px-3 py-2 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500"
               />
+              @error('email')
+              <div class="mt-1 text-red-500">{{ $message }}</div>
+          @enderror
             </div>
             <!-- END Email Input -->
           
             <!-- Password Input -->
-            <div class="space-y-1">
-              <label for="password" class="font-medium">Phone Number</label>
-              <input
-                type="number"
-                
-                name="password"
-                placeholder="Enter your password"
-                class="block w-full rounded-lg border border-gray-200 px-3 py-2 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500"
-              />
-            </div>
-            <div class="space-y-1">
-                <label for="NID" class="font-medium">NID</label>
-                <input
-                  type="number"
-                
-                  name="nid"
-                  placeholder="Enter your national id number"
-                  class="block w-full rounded-lg border border-gray-200 px-3 py-2 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500"
-                />
-              </div>
+            
+            
+            
             <!-- END Password Input -->
-            <div class="space-y-1">
+            {{-- <div class="space-y-1">
                 
 <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-<select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+<select wire-model="vaccine_centre_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     <option selected>Choose a country</option>
     @foreach ($vaccineCentres as $vaccineCentre)
     <option value="{{$vaccineCentre->id}}">{{$vaccineCentre->name}}</option>
@@ -64,14 +56,17 @@
   
   
 </select>
-
-            </div>
+@error('nid')
+              <div class="mt-1 text-red-500">{{ $message }}</div>
+                @enderror
+            </div> --}}
             <button
     type="submit"
     class="inline-flex items-center justify-center space-x-2 rounded-lg border border-blue-700 bg-blue-700 px-3 py-2 text-sm font-semibold leading-5 text-white hover:border-blue-600 hover:bg-blue-600 hover:text-white focus:ring focus:ring-blue-400 focus:ring-opacity-50 active:border-blue-700 active:bg-blue-700 dark:focus:ring-blue-400 dark:focus:ring-opacity-90"
   >
     Submit
   </button>
+  
           </form>
     </div>
 
