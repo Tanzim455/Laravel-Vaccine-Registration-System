6eae98e4ecebafa489080cee4458d7deccbadea1
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-01-18 14:22:15 +0600

    Email configured

--- app/Livewire/UserRegistration.php
+++ app/Livewire/UserRegistration.php
@@ -143,3 +143,4 @@ protected function rules(): array
     
     
 
+   
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Jobs\VaccineScheduleJob;
 use App\Livewire\UserRegistration;
+use App\Mail\VaccineSchedule;
 use App\Models\User;
 use App\Models\VaccineCentre;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,49 +21,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // $date_greater_than_scheduled_day_exists = User::where('vaccine_centre_id', 2)
-    // ->where('scheduled_date', '>', '2024-01-17')
-    // ->exists();
-
-// if ($date_greater_than_scheduled_day_exists) {
-//     dd("Loop Through on");
-// } else {
-//     dd("It does not exist.Just add one more day ");
-// }
-
-    // User::findOrFail(1)->update([
-    //   'scheduled_date'=>'2024-01-17'
-    // ]);
-    // dump(User::findOrFail(1));
-  // $date=Carbon::parse($user->created_at)->addDay(1)->format('Y-m-d');
-  //    dump($date);
-  //    dump(Carbon::parse($date)->format('l')==='Tuesday ');
-//   $vaccine_centre_limit=VaccineCentre::findOrFail(2)->daily_limit;
-//   dump($vaccine_centre_limit);
-// $collection = collect([10,40,60,90]);
-// dd($collection->max());
-$user=User::select('vaccine_centre_id', 'scheduled_date')
-->where('vaccine_centre_id', 2)
-->where('scheduled_date','>',Carbon::parse(Carbon::now())->format('Y-m-d'))
-->groupBy('vaccine_centre_id', 'scheduled_date')
-->selectRaw('COUNT(*) as count')
-->get();
- dump($user);
- //Find vaccine centre limit 
- 
-    //   foreach($user as $u){
-    //     // $array=[];
-    //     // array_push($array,$u->scheduled_date);
-    //     // dd($array);
-    //     $collection = collect([]);
-    //     $collection->push($u->scheduled_date);
-    //     dd($collection);
-    //   }
-$date=Carbon::now();
-dump(Carbon::parse($date)->format('Y-m-d'));
-//Add one day
-$add_one_day=Carbon::parse($date)->addDays(1)->format('Y-m-d');
-dd($add_one_day);
-// dump($add_one_day<$date);
+    // $day = Carbon::parse(Carbon::now()->format('Y-m-d'));
+    //     //Find all schedule dates greater than current date
+    //     $scheduled_dates = User::where('scheduled_date', '>', $day)->pluck('scheduled_date');
+    //     // dd($scheduled_dates);
+    //     //Find earliest date from them
+    //      $min=$scheduled_dates->min();
+         
+    //     //  dd($min);
+    //      //Find out all users who are scheduled earlist from today
+    //      $users=User::where('is_scheduled',0)
+    //      ->where('scheduled_date','=',$min)
+    //      ->get();
+    //      dd($users);
+          
+    //      foreach($users as $user){
+    //         Mail::to($user->email)->queue(new VaccineSchedule(user:$user));
+    //         $user->is_scheduled=true;
+    //         $user->save();
+    //      }
+    VaccineScheduleJob::dispatch();
+   
   });
  Route::get('register',UserRegistration::class);
